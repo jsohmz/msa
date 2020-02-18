ab6803b18f6809c11204e21bfad67807b45b4951
ojs <jsoh@mz.co.kr>
2020-02-18 17:56:08 +0900

    MSA Study gateway service - zuul config 추가

--- gateway-service/src/main/kotlin/GatewayService.kt
+++ gateway-service/src/main/kotlin/GatewayService.kt
@@ -3,8 +3,10 @@ package msa.gateway
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 class GatewayService
 
